fe4d1e76843ab6b39e0a978013cb3212d1c13128
César <cesarantonio.r@students.salle.url.edu>
2025-12-23 21:33:45 +0100

    Some of the bruteForce done

--- src/src/Node.java
+++ src/src/Node.java
@@ -5,21 +5,46 @@ public class Node {
     int time;
     int questsCompleted;
 
-    public Node(ArrayList<Quest> quests, int questsCompleted, int time) {
-        this.quests = quests;
-        this.questsCompleted = questsCompleted;
-        this.time = time;
+    public Node () {
+        this.quests = new ArrayList<>();
+        this.time = 0;
+        this.questsCompleted = 0;
+    }
+
+    public Node(Node that) {
+        this.quests = that.quests;
+        this.questsCompleted = that.questsCompleted;
+        this.time = that.time;
     }
 
     public void addToQuests (Quest quest) {
         this.quests.add(quest);
     }
 
-    public void updateQuestsCompleted (int numQuestCompleted) {
-        this.questsCompleted += numQuestCompleted;
+    public void updateQuestsCompleted (Quest quest) {
+        if(quest.getImportance().equals("#4FD945")) {
+            this.questsCompleted++;
+        } else if(quest.getImportance().equals("#CC00FF")) {
+            this.questsCompleted += 2;
+        } else if (quest.getImportance().equals("#FF8000")) {
+            this.questsCompleted += 5;
+        }
     }
 
-    public void updateTime (int timeToBeAdd) {
-        this.time += timeToBeAdd;
+    public void updateTime (Quest quest) { this.time += quest.getEstimatedTime(); }
+
+    public void calculateTotalTime () {
+        float discount = 0;
+        for(Quest q : this.quests) {
+            for (Quest q2 : this.quests) {
+                if (q.getSubject().equals(q2.getSubject()) && q != q2)  {
+                    discount += (float) (q.getEstimatedTime() * 0.1);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("\nDiscounted time for next one: " + discount);
+        this.time -= Math.round(discount);
     }
 }
--- src/src/TheWallOfDeadlines.java
+++ src/src/TheWallOfDeadlines.java
@@ -1,32 +1,54 @@
 import java.io.FileNotFoundException;
 
 public class TheWallOfDeadlines {
+    int maxTime;
 
-    private void recursive(int[] config, int level) throws FileNotFoundException {
-        for (int i = 0; i < Data.numQuests(); i++) {
-            if (level < 3) {
-                config[level] = i;
-                recursive(config, level + 1 );
+    private void bruteForce(Quest[] quests, int level, Node node) {
+        // Final call
+        if (level == quests.length) {
+            node.calculateTotalTime();
+            if (node.time <= maxTime) {
+                printConfig(node);
             }
-            else {
-                printConfig(config);
-                return;
-            }
+            return;
         }
+
+        // Skip quest
+        bruteForce(quests, level + 1, node);
+
+        // Store quest in a new node
+        Quest quest = quests[level];
+        Node newNode = new Node(node);
+
+        newNode.addToQuests(quest);
+        newNode.updateTime(quest);
+        newNode.updateQuestsCompleted(quest);
+
+        if (newNode.time <= maxTime) {
+            bruteForce(quests, level + 1, newNode);
+        }
+
+        newNode.quests.removeLast();
     }
 
-    private void printConfig(int[] config) {
-        System.out.print("[ ");
-        for (int i = 0; i < config.length; i++) {
-            System.out.print(config[i] + " ");
+    private void printConfig(Node node) {
+
+        System.out.println("\n--------------------------");
+        for (int i = 0; i < node.quests.size(); i++) {
+            System.out.println(node.quests.get(i).getEstimatedTime() +
+                    "  \t|    " + node.quests.get(i).getSubject() +
+                    "    |    " + node.quests.get(i).getName());
         }
-        System.out.print("]");
-        System.out.println();
+        System.out.println("\nTotal time: " + node.time + "\nQuest completed: " + node.questsCompleted);
+        System.out.println("--------------------------");
     }
 
-    public void brutForce() throws FileNotFoundException {
-        int[] config = new int[3];
+    public void startBruteForce() throws FileNotFoundException {
+        Node node = new Node();
+        Data data = new Data();
+        Quest[] quests = data.creatListQuests();
 
-        recursive(config, 0);
+
+        bruteForce(quests, 0, node);
     }
 }
