2e3402eb1961c5f317178188a6cd39e7932f0c69
Axel <a.boleda@students.salle.url.edu>
2025-12-23 11:36:50 +0100

    Menu class added

---
+++ src/src/Menu.java
@@ -0,0 +1,8 @@
+public class Menu {
+
+    public void showProblems() {
+        System.out.println("Chose which problem do you want to solve");
+        System.out.println("\tThe wall of deadlines");
+        System.out.println("\tThe festival of infinite quests");
+    }
+}
