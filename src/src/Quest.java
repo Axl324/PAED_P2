dbe8710a05dfc094348896aa086131dd60fe4064
Axel <a.boleda@students.salle.url.edu>
2025-12-23 11:32:10 +0100

    Read from data

--- src/src/Quest.java
+++ src/src/Quest.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Quest {
+    private String name;
+    private String subject;
+    private LocalDate deadline;
+    private int estimatedTime;
+    private int difficulty;
+    private int progress;
+    private String importance;
+    private String location;
+
+    // Class of quest.
+    public Quest(String name, String subject, LocalDate deadline, int estimatedTime, int difficulty, int progress, String importance, String location) {
+        this.name = name;
+        this.subject = subject;
+        this.deadline = deadline;
+        this.estimatedTime = estimatedTime;
+        this.difficulty = difficulty;
+        this.progress = progress;
+        this.importance = importance;
+        this.location = location;
+    }
+
+    // Functions to get information from the class.
+    public String getName() {return name;}
+    public LocalDate getDeadline() {return deadline;}
+    public int getEstimatedTime() {return estimatedTime;}
+    public int getDifficulty() {return difficulty;}
+    public int getProgress() {return progress;}
+
 }
